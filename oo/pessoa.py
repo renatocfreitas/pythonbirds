b88ea6c0429998deb52935c5ad9697500502246f
Renato Freitas <renatocfreitas@gmail.com>
2021-01-05 16:23:33 -0300

    Criados atributos de instancia e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,15 @@
 class Pessoa:
+
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Renato')
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
