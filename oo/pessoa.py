79c65763ac3dc41122c24501988854347bae5fa3
Renato Freitas <renatocfreitas@gmail.com>
2021-01-06 10:44:29 -0300

    Criado atributo de classe olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,6 @@
 class Pessoa:
-
+    # atributo default ou de classe - definido fora do __init__
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
@@ -12,14 +13,18 @@ if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
 
-    print(id(luciano))
+    print(id(luciano))            #id do objeto
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
 
-    luciano.sobrenome = 'Ramalho'
-    del luciano.filhos
-    print(luciano.__dict__)
+    luciano.sobrenome = 'Ramalho' #criacao dinamica de metodo
+    del luciano.filhos            #delecao dinamica de metodo
+    luciano.olhos = 1             #atributo de classe modificado (apenas) no escopo do objeto
+    print(luciano.__dict__) #conteudo do objeto listado como dicionario
     print(renzo.__dict__)
+    print(Pessoa.olhos)     #atributo de classe chamado pela Classe
+    print(renzo.olhos)      #atributo de classe chamado pelo objeto
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos)) # ids
